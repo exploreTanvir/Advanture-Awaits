512e15feac75475b1e359006286e69b49ecfd604
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2024-04-06 13:04:51 +0600

    Add files via upload

---
+++ src/components/data.jsx
@@ -0,0 +1,19 @@
+const data=[
+    {
+        title:"For you and your family",
+        text:"We protect vehicles, property, pets and events.",
+        icon:'fa-solid fa-user-group'
+    },
+    {
+        title:"For your business",
+        text:"We protect businesses and employees, as well as company property and vehicles.",
+        icon:"fa-solid fa-business-time"
+    },
+    {
+        title:"For your future",
+        text:"We protect investments, retirements and financial dreams.",
+        icon:'fa-solid fa-lightbulb'
+    },
+]
+
+export default data;
