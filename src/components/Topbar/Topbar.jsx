f2ba5154e0b339a6f79a6df6a9c642bc862e0c3a
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2024-04-07 16:57:44 +0600

    Add files via upload

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -7,7 +7,7 @@ const TopBar = ({menuOpen,setMenuOpen}) => {
       <div className='wrapper'>
         <div className='left'>
           <a href='#intro' className='logo'>
-            TANVIR
+            MTHR
           </a>
           
         </div>
---
+++ src/components/showDown/Show.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+import "./show.css"
+
+const Show = () => {
+  return (
+    <div className='show'>
+        <div className="item">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Show
