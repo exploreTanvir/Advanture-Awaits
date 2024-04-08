4f547877ec56fdde1cb1e1c4dd79223764f0513d
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2024-04-08 16:16:44 +0600

    Add files via upload

---
+++ src/components/company/Company.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./company.css"
+
+const Company = () => {
+  return (
+    <div className='company'>
+        <div className="left">
+            <h1>The Best <i>Insurance</i> Companies for 2021 </h1>
+
+        </div>
+        <div className="right">
+       <p> you're in <span> good hands</span></p>
+        </div>
+    </div>
+  )
+}
+
+export default Company
