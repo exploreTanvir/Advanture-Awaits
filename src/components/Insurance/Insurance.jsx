d20c47f424241362b792c405f86b73c167ae6d7b
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2024-04-09 23:00:50 +0600

    Add files via upload

--- src/components/Insurance/Insurance.jsx
+++ src/components/Insurance/Insurance.jsx
@@ -7,7 +7,7 @@ const Insurance = () => {
         <div className="left">
             <h1>The Best Car Insurance Companies for 2021 </h1>
             <u>We looked at claims processes, transparency, complaints and more to find and rank the best car insurance companies.</u>
-            <p>Many or all of the products featured here are from our partners who compensate us. This may influence which products we write about and where and how the product appears on a page. However, this does not influence our evaluations. Our opinions are our own.</p>
+            <p className='first'>Many or all of the products featured here are from our partners who compensate us. This may influence which products we write about and where and how the product appears on a page. However, this does not influence our evaluations. Our opinions are our own.</p>
             <p>The best car insurance companies give their customers more than just coverage they want at a reasonable price — merely good companies can do that. On top of that, the best auto insurance companies provide a customer-first experience from quote to claim, including a headache-free way to manage policies and make payments.</p>
         </div>
 
